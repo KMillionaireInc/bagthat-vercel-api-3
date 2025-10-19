d831a22b646835f96598ea08226d5964840e6689
KMillionaireInc <hello@kmillionaireinc.com>
2025-10-19 11:53:14 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: 'API is live and working!' });
-}
+return Response.json({
+  success: true,
+  tenant_id: data.tenant_id,
+  site_display_name: data.site_display_name,
+  subdomain: data.subdomain,
+  amazon_tag: data.amazon_tag,
+  email: data.email,
+});
